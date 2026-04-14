53401fac30151843cd6ac3962d37216e0bc8f34d
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-04-14 16:43:24 +0530

    add user account

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -19,6 +19,7 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         if (!user) {
             
             throw new ApiError(401, "Invalid Access Token")
+            
         }
     
         req.user = user;
